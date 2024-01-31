7beea0f03f0d88881bc4b74bc8c1312582d00ce6
sakhb <druzhininsergey96@gmail.com>
2024-02-01 08:54:40 +1100

    learn words branch realization

--- src/main/kotlin/words_file.kt
+++ src/main/kotlin/words_file.kt
@@ -19,14 +19,30 @@ fun main() {
         val word = Word(original = line[0], translated = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
         dictionary.add(word)
     }
+    val learnedWordsCount = dictionary.filter { it.correctAnswersCount >= REQUIRED_CORRECT_ANSWERS }
+    val unlearnedWords = dictionary.filter { it.correctAnswersCount < REQUIRED_CORRECT_ANSWERS }
 
     while (true) {
         println("Меню: 1- Учить слова, 2 - Статистика, 0 - Выход")
-        val input = readln().toIntOrNull()
-        when (input) {
-            1 -> println("Выбран пункт \"Учить слова\".")
+        val menuInput = readln().toIntOrNull()
+        when (menuInput) {
+            1 -> {
+                if (learnedWordsCount.size == lines.size) {
+                    println("Вы выучили все слова.")
+                    continue
+                }
+                val fourShuffledWords = unlearnedWords.shuffled().take(4)
+                val correctWord = fourShuffledWords.random()
+                var resultsCounter = 0
+
+                println(correctWord.original)
+                for (i in fourShuffledWords.shuffled()) {
+                    resultsCounter++
+                    println("${resultsCounter}) ${i.translated}")
+                }
+            }
+
             2 -> {
-                val learnedWordsCount = dictionary.filter { it.correctAnswersCount >= REQUIRED_CORRECT_ANSWERS }
                 val learnedWordInPercent = ((learnedWordsCount.size.toDouble() / lines.size) * 100).toInt()
                 println("Выучено ${learnedWordsCount.size} из ${lines.size} слов | $learnedWordInPercent%")
             }
