f310407bc921e3eb952ae102ec12748ae351977c
sakhb <druzhininsergey96@gmail.com>
2024-02-02 08:00:31 +1100

    Supplemented incomplete list with learned words

--- src/main/kotlin/words_file.kt
+++ src/main/kotlin/words_file.kt
@@ -1,6 +1,7 @@
 import java.io.File
 
 const val REQUIRED_CORRECT_ANSWERS = 3
+const val NUMBER_OF_ANSWER_OPTIONS = 4
 
 data class Word(
     val original: String,
@@ -19,7 +20,6 @@ fun main() {
         val word = Word(original = line[0], translated = line[1], correctAnswersCount = line[2].toIntOrNull() ?: 0)
         dictionary.add(word)
     }
-    val learnedWordsCount = dictionary.filter { it.correctAnswersCount >= REQUIRED_CORRECT_ANSWERS }
     val unlearnedWords = dictionary.filter { it.correctAnswersCount < REQUIRED_CORRECT_ANSWERS }
 
     while (true) {
@@ -27,24 +27,29 @@ fun main() {
         val menuInput = readln().toIntOrNull()
         when (menuInput) {
             1 -> {
-                if (learnedWordsCount.size == lines.size) {
+                if (unlearnedWords.isEmpty()) {
                     println("Вы выучили все слова.")
                     continue
                 }
-                val fourShuffledWords = unlearnedWords.shuffled().take(4)
-                val correctWord = fourShuffledWords.random()
-                var resultsCounter = 0
+                val fourShuffledWords = if (unlearnedWords.size >= 4) {
+                    unlearnedWords.shuffled().take(4)
+                } else {
+                    val learnedWords = dictionary - unlearnedWords.toSet()
+                    val missingWords = NUMBER_OF_ANSWER_OPTIONS - unlearnedWords.size
+                    unlearnedWords + learnedWords.shuffled().take(missingWords)
+                }
+                val correctWord = fourShuffledWords.filter { it in unlearnedWords }.random()
 
                 println(correctWord.original)
-                for (i in fourShuffledWords.shuffled()) {
-                    resultsCounter++
-                    println("${resultsCounter}) ${i.translated}")
+                fourShuffledWords.shuffled().forEachIndexed { index, word ->
+                    println("${index + 1}) ${word.translated}")
                 }
             }
 
             2 -> {
-                val learnedWordInPercent = ((learnedWordsCount.size.toDouble() / lines.size) * 100).toInt()
-                println("Выучено ${learnedWordsCount.size} из ${lines.size} слов | $learnedWordInPercent%")
+                val learnedWordInPercent =
+                    (((dictionary.size - unlearnedWords.size).toDouble() / dictionary.size) * 100).toInt()
+                println("Выучено ${dictionary.size - unlearnedWords.size} из ${dictionary.size} слов | $learnedWordInPercent%")
             }
 
             0 -> {
