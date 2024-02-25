da961a26f929195e5685505c2c03f4ccd1d955b6
sakhb <druzhininsergey96@gmail.com>
2024-02-26 08:25:28 +1100

    add shadowJar plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     kotlin("plugin.serialization") version "1.9.22"
+    application
 }
 
 group = "org.example"
@@ -22,3 +24,6 @@ kotlin {
     jvmToolchain(19)
 }
 
+application{
+    mainClass.set("TelegramKt")
+}
